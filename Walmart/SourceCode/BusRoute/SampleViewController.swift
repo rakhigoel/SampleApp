96c2dc1c3f5346be4f46ccc73fc86740834f6899
Rakhi goel <>
2017-11-29 22:06:41 -0800

    sample

---
+++ Walmart/SourceCode/BusRoute/SampleViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SampleViewController.swift
+//  BusRoute
+//
+//  Created by Rakhi goel on 11/29/17.
+//  Copyright © 2017 Rakhi goel. All rights reserved.
+//
+
+import UIKit
+
+class SampleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
